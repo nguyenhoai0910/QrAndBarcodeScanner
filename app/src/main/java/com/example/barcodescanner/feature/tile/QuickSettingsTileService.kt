e85f999c508dc76f82f4b27d3bec034afa514d21
NguyenHoaiZoo <you@example.com>
2026-07-29 16:30:57 +0700

    update-20260729-163057-5333

--- app/src/main/java/com/example/barcodescanner/feature/tile/QuickSettingsTileService.kt
+++ app/src/main/java/com/example/barcodescanner/feature/tile/QuickSettingsTileService.kt
@@ -1,5 +1,6 @@
 package com.example.barcodescanner.feature.tile
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.TileService
@@ -14,6 +15,17 @@ class QuickSettingsTileService : TileService() {
         val intent = Intent(applicationContext, BottomTabsActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
         }
-        startActivityAndCollapse(intent)
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(pendingIntent)
+        } else {
+            @Suppress("DEPRECATION")
+            startActivityAndCollapse(intent)
+        }
     }
 }
--- app/src/main/java/com/example/barcodescanner/usecase/Logger.kt
+++ app/src/main/java/com/example/barcodescanner/usecase/Logger.kt
@@ -1,14 +1,14 @@
 package com.example.barcodescanner.usecase
 
+import android.util.Log
 import com.example.barcodescanner.BuildConfig
-import io.sentry.Sentry
 
 object Logger {
     var isEnabled = BuildConfig.ERROR_REPORTS_ENABLED_BY_DEFAULT
 
     fun log(error: Throwable) {
         if (isEnabled) {
-            Sentry.captureException(error)
+            Log.e("BarcodeScanner", "Unhandled error", error)
         }
     }
 }
